6641e92d023121ddb2d951ddabd75c907a1c25e8
paladin <salavtoreshsf@163.com>
2023-10-18 17:11:14 +0800

    spring05:get singleton bean from cache

--- src/main/java/com/service/UserService.java
+++ src/main/java/com/service/UserService.java
@@ -3,16 +3,16 @@ package com.service;
 import org.springframework.stereotype.Service;
 
 /**
- * @title: UserService
- * @Author Smiling-HuYang
- * @Version 1.0
+ * @name UserService
+ * @author Smiling-HuYang
+ * @version 1.0
  */
 @Service
 public class UserService {
 
     public UserService() {
         //创建对象会调用构造方法
-        throw new RuntimeException("人为地制造一个异常.....");
+//        throw new RuntimeException("人为地制造一个异常.....");
     }
 
     public void save(){
